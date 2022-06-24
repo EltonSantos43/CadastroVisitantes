83df78808fd8b2f74994eeed021ea2642cbfb93e
Elton Santos <elton.c.s1979@gmail.com>
2022-06-24 20:58:43 -0300

    Cadastro de Visitantes

---
+++ visitantes/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from visitantes.models import Visitante
+
+
+class VisitanteForm(forms.ModelForm):
+    class Meta:
+        model = Visitante
+        fields = [
+            "nome_completo", "cpf", "data_nascimento",
+            "numero_casa", "placa_veiculo"
+        ]
+
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para o registro."
+            },
+            "cpf": {
+                "required": "O CPF do visitante é obrigatório para o registro."
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatório para o registro.",
+                "invalid": "Por favor, informe um formato de data válido (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "Por favor, informe o número da casa  a ser visitada."
+            }
+
+        }
+
+
+class AutorizaVisitanteForm(forms.ModelForm):
+    morador_responsavel = forms.CharField(required=True)
+
+    class Meta:
+        model = Visitante
+        fields = [
+            "morador_responsavel"
+        ]
+        error_messages = {
+            "morador_responsavel": {
+                "required": "Nome do morador responsável é obrigatório."
+            }
+        }
+
+
