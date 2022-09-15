d35ad9df79eaf617ec72e25246699792b1c96016
Elton Santos <elton.c.s1979@gmail.com>
2022-09-15 10:45:25 -0300

    ok

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%=sh4n^0figrf#814j6y_u!b4@ljw@%1#0j$l2itkj1e@r6ac-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
